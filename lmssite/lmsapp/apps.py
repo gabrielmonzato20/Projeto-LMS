8c69ceac01a707ad32c7e79b78361f55676cf8de
Gabriel Monzato Grigorio <1800613@salas.aulas>
2018-10-31 11:24:52 -0300

    Alterando os apps

--- lmssite/lmsapp/apps.py
+++ lmssite/lmsapp/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class LmsappConfig(AppConfig):
-    name = 'lmsapp'
+    name = 'LMSAPP'
--- lmssite/lmsapp/views.py
+++ lmssite/lmsapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def aluno(request):
+    return render(request, 'aluno.html')
+
+def disciplina(request):
+    return render(request, 'disciplina.html')
+
+def professor(request):
+    return render(request, 'professor.html')
+
